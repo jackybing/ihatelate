8431391a1dd8bebf7657d58199d917f82789fc35
Pandaroid <Payne.Pandaroid.Wang@foxmail.com>
2013-11-22 17:41:37 +0800

    jumpAction添加新的跳转

--- ihatelate/src/com/ihl/controller/jumpaction/JumpAction.java
+++ ihatelate/src/com/ihl/controller/jumpaction/JumpAction.java
@@ -1,6 +1,12 @@
 package com.ihl.controller.jumpaction;
 
 public class JumpAction {
+	public String viewSchedule() {
+		return "viewSchedule";
+	}
+	public String idleTimeCalendar() {
+		return "idleTimeCalendar";
+	}
 	public String index() {
 		return "index";
 	}
