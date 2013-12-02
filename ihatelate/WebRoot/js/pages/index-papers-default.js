1a1faaedc8a18abe346cd01e7de94c9c4f25508d
Pandaroid <Payne.Pandaroid.Wang@foxmail.com>
2013-12-02 22:29:22 +0800

    论文非量化主体表单的元素获取和检查完成 接下来是stages的检查

--- ihatelate/WebRoot/js/pages/index-papers-default.js
+++ ihatelate/WebRoot/js/pages/index-papers-default.js
@@ -1,15 +1,118 @@
 (function($) {
 	$(function() {
+		// scroll to element
+		var isInScrolling = false;
+        function scrollToElement(targetEle) {
+            if(!isInScrolling) {
+                isInScrolling = true;
+                var anh = targetEle.offset().top - 95;
+                /*console.log(anh);*/
+                $("html,body").stop().animate({scrollTop: anh}, 
+                    { 
+                        duration: 500, 
+                        queue: false, 
+                        complete: function() { 
+                            isInScrolling = false;
+                            // targetEle.focus().select();
+                        } 
+                    }
+                );
+            }
+        }
+		// 展示error tip的function
+		function showErrorTip(element, msg) {
+			scrollToElement(element);
+			element.parents(".control-group").addClass("error").find(".help-inline").text(msg);
+		}
+		// function to clear error tips
+		function clearMyErrorTip(myself) {
+            var myControlGroupParent = myself.parents(".control-group");
+            if(myControlGroupParent.hasClass("error")) {
+                myControlGroupParent.removeClass("error").find(".help-inline").text("");
+            }
+        }
+		// clear error tip for controls
+		$("input").on("input", function() {
+            clearMyErrorTip($(this));
+        });
+        $("input").on("change", function() {
+            clearMyErrorTip($(this));
+        });
+        // function to compute date range days
+        function computeDateRangeDays(sDate1, sDate2) {	//sDate1和sDate2是2002-12-18格式 
+        	var aDate, oDate1, oDate2, iDays  
+	        aDate = sDate1.split("-")  
+	        oDate1 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0])  //转换为12-18-2002格式 
+	        aDate = sDate2.split("-") 
+	        oDate2 = new Date(aDate[1] + '-' + aDate[2] + '-' + aDate[0]) 
+	        iDays = parseInt(Math.abs(oDate1 - oDate2) / 1000 / 60 / 60 /24)  //把相差的毫秒数转换为天数 
+	        return iDays 
+        }
+        
+		// 通过事件委托绑定事件
 		$(document).on("click", ".add-non-quant-task-btn", function() {
 			var this_btn = $(this), item = this_btn.data("item"), type = this_btn.data("type");
 			console.log("item: " + item + "; type: " + type);
 			// 先获取所有输入控件
-			
+			var task_name_element = $("#" + item + "-task-name-" + type), 
+				start_time_element = $("#" + item + "-start-time-" + type),
+				end_time_element = $("#" + item + "-end-time-" + type),
+				total_day_element = $("#" + item + "-total-day-" + type),
+				paper_name_element = $("#" + item + "-paper-name-" + type),
+				is_active_element = $("#" + item + "-is-active-" + type);
 			// 再获取所有输入控件的值（去掉前后空格）
-			
+			var task_name = $.trim(task_name_element.val()), start_time = $.trim(start_time_element.val()),
+				end_time = $.trim(end_time_element.val()), total_day = $.trim(total_day_element.val()),
+				paper_name = $.trim(paper_name_element.val()),
+				is_active = is_active_element.parent().hasClass("switch-on") == true ? "1" : "0";;
+			console.log("task_name: " + task_name + "; start_time: " + start_time + "; end_time: " + end_time);
+			console.log("total_day: " + total_day + "; paper_name: " + paper_name + "; is_active: " + is_active);
 			// 对输入控件的值进行检测，如果不对，显示error tip
-			
+			var is_validate = true;
+			if(task_name == "") {
+				showErrorTip(task_name_element, "Please input a task name");
+				is_validate = false;
+			}
+			if(start_time == "" && end_time == "") {
+				showErrorTip(start_time_element, "Please input a start date and an end date");
+				is_validate = false;
+			} else if(start_time == "") {
+				showErrorTip(start_time_element, "Please input a start date");
+				is_validate = false;
+			} else if(end_time == "") {
+				showErrorTip(end_time_element, "Please input an end date");
+				is_validate = false;
+			}
+			if(start_time == "" || end_time == "") {
+				showErrorTip(total_day_element, "Please input Date Range first");
+				is_validate = false;
+			} else if(total_day == "") {
+				showErrorTip(total_day_element, "Please input days you plan to spend on this task");
+				is_validate = false;
+			} else if(isNaN(total_day)) {
+				showErrorTip(total_day_element, "Please input an integer");
+				is_validate = false;
+			} else if(parseInt(total_day) != parseFloat(total_day)) {
+				showErrorTip(total_day_element, "Please input an integer, not a float");
+				is_validate = false;
+			} else {
+				total_day = parseInt(total_day);
+				total_day_element.val(total_day).trigger("change");
+				var date_range_days = computeDateRangeDays(start_time, end_time);
+				if(total_day < 1) {
+					showErrorTip(total_day_element, "You must cost at least 1 day on this task");
+					is_validate = false;
+				} else if(total_day > date_range_days) {
+					showErrorTip(total_day_element, "You must cost no more than " + date_range_days + " days (on date range)");
+					is_validate = false;
+				}
+			}
+			if(paper_name == "") {
+				showErrorTip(paper_name_element, "Please input a paper name");
+				is_validate = false;
+			}
 			// 通过检测，则ajax提交结果，提交成功后触发reset事件
+			
 			
 		}).on("click", ".add-new-stage-btn", function() {
 			var this_element = $(this), wrapper_id = this_element.data("wrapperId"),
