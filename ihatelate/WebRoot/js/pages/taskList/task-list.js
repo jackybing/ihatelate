a1461bd7c7745a9e340c76f2b5895980d953357a
Pandaroid <Payne.Pandaroid.Wang@foxmail.com>
2014-01-28 22:14:20 +0800

    优化TaskListHelper的genTaskTbTr接口结构和效率 完成按ID删除单个任务接口

--- ihatelate/WebRoot/js/pages/taskList/task-list.js
+++ ihatelate/WebRoot/js/pages/taskList/task-list.js
@@ -12,101 +12,77 @@
 		initTlTable();
 		
 		var TaskListHelper = {
+			genTaskTbTrBtns: function(task_id, task_name) {
+				var ret_html_array = [];
+				ret_html_array.push(
+				'<div class="btn-group">',
+	          		'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
+		        	'<button class="btn btn-danger btn-mini delete-task-btn" title="Delete Task Information" data-name="', task_name, '" data-id="', task_id, '"><i class="icon-trash"></i></button>',
+	          	'</div>');
+				return ret_html_array.join("");
+			},
 			genTaskTbTr: function(task, idx) {
-				var ret_html_array = [], task_type = task.type, idx = parseInt(idx), tr_class = (idx % 2) ? "oddrow" : "evenrow", tr_idx = idx + 1,
+				var this_ptr = this, ret_html_array = [], task_type = task.type, idx = parseInt(idx), 
+					tr_class = (idx % 2) ? "oddrow" : "evenrow", tr_idx = idx + 1, task_name = task.name,
 					is_active_tr_html = task.isActive ? '<td class="highlight-success is-active-td"><i class="icon-ok"></i></td>' : '<td class="highlight-danger is-active-td"><i class="icon-remove"></i></td>',
 					start_time = task.startTime.substring(0, 10), end_time = task.endTime.substring(0, 10);
+				ret_html_array.push('<tr class="', tr_class, '">');
 				if(task_type == "10") {	// Read Book
 					ret_html_array.push(
-					'<tr class="', tr_class, '">',
-	          			'<td>', tr_idx, '</td>',
-	          			'<td>', task.name, '</td>',
-	          			'<td>', start_time, '</td>',
-	          			'<td>', end_time, '</td>',
-	          			'<td>', task.totalDay, '</td>',
-	          			is_active_tr_html,
-	          			'<td>Reading</td>',
-	          			'<td>', task.title, '</td>',
-	          			'<td>',
-	          				'<div class="btn-group">',
-	          					'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
-		          				'<button class="btn btn-danger btn-mini" title="Delete Task Information"><i class="icon-trash"></i></button>',
-	          				'</div>',
-	          			'</td>',
-	          		'</tr>');
+          			'<td>', tr_idx, '</td>',
+          			'<td>', task_name, '</td>',
+          			'<td>', start_time, '</td>',
+          			'<td>', end_time, '</td>',
+          			'<td>', task.totalDay, '</td>',
+          			is_active_tr_html,
+          			'<td>Reading</td>',
+          			'<td>', task.title, '</td>');
 				} else if(task_type == "11") {	// Open Class
 					ret_html_array.push(
-					'<tr class="', tr_class, '">',
-	          			'<td>', tr_idx, '</td>',
-	          			'<td>', task.name, '</td>',
-	          			'<td>', start_time, '</td>',
-	          			'<td>', end_time, '</td>',
-	          			'<td>', task.totalDay, '</td>',
-	          			is_active_tr_html,
-	          			'<td>Open Class</td>',
-	          			'<td>', task.className, '</td>',
-	          			'<td>',
-	          				'<div class="btn-group">',
-	          					'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
-		          				'<button class="btn btn-danger btn-mini" title="Delete Task Information"><i class="icon-trash"></i></button>',
-	          				'</div>',
-	          			'</td>',
-	          		'</tr>');
+          			'<td>', tr_idx, '</td>',
+          			'<td>', task_name, '</td>',
+          			'<td>', start_time, '</td>',
+          			'<td>', end_time, '</td>',
+          			'<td>', task.totalDay, '</td>',
+          			is_active_tr_html,
+          			'<td>Open Class</td>',
+          			'<td>', task.className, '</td>');
 				} else if(task_type == "12") {	// 健身
 					ret_html_array.push(
-					'<tr class="', tr_class, '">',
-	          			'<td>', tr_idx, '</td>',
-	          			'<td>', task.name, '</td>',
-	          			'<td>', start_time, '</td>',
-	          			'<td>', end_time, '</td>',
-	          			'<td>', task.totalDay, '</td>',
-	          			is_active_tr_html,
-	          			'<td>Exercise</td>',
-	          			'<td>', task.exerciseName, '</td>',
-	          			'<td>',
-	          				'<div class="btn-group">',
-	          					'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
-		          				'<button class="btn btn-danger btn-mini" title="Delete Task Information"><i class="icon-trash"></i></button>',
-	          				'</div>',
-	          			'</td>',
-	          		'</tr>');
+          			'<td>', tr_idx, '</td>',
+          			'<td>', task_name, '</td>',
+          			'<td>', start_time, '</td>',
+          			'<td>', end_time, '</td>',
+          			'<td>', task.totalDay, '</td>',
+          			is_active_tr_html,
+          			'<td>Exercise</td>',
+          			'<td>', task.exerciseName, '</td>');
 				} else if(task_type == "20") {	// 写论文
 					ret_html_array.push(
-					'<tr class="', tr_class, '">',
-	          			'<td>', tr_idx, '</td>',
-	          			'<td>', task.name, '</td>',
-	          			'<td>', start_time, '</td>',
-	          			'<td>', end_time, '</td>',
-	          			'<td>', task.totalDay, '</td>',
-	          			is_active_tr_html,
-	          			'<td>Paper</td>',
-	          			'<td>', task.paperName, '</td>',
-	          			'<td>',
-	          				'<div class="btn-group">',
-	          					'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
-		          				'<button class="btn btn-danger btn-mini" title="Delete Task Information"><i class="icon-trash"></i></button>',
-	          				'</div>',
-	          			'</td>',
-	          		'</tr>');
+          			'<td>', tr_idx, '</td>',
+          			'<td>', task_name, '</td>',
+          			'<td>', start_time, '</td>',
+          			'<td>', end_time, '</td>',
+          			'<td>', task.totalDay, '</td>',
+          			is_active_tr_html,
+          			'<td>Paper</td>',
+          			'<td>', task.paperName, '</td>');
 				} else if(task_type == "21") {	// 申请大学
 					ret_html_array.push(
-					'<tr class="', tr_class, '">',
-	          			'<td>', tr_idx, '</td>',
-	          			'<td>', task.name, '</td>',
-	          			'<td>', start_time, '</td>',
-	          			'<td>', end_time, '</td>',
-	          			'<td>', task.totalDay, '</td>',
-	          			is_active_tr_html,
-	          			'<td>University</td>',
-	          			'<td>', task.universityName, '</td>',
-	          			'<td>',
-	          				'<div class="btn-group">',
-	          					'<button class="btn btn-info btn-mini" title="Edit Task Information"><i class="icon-edit"></i></button>',
-		          				'<button class="btn btn-danger btn-mini" title="Delete Task Information"><i class="icon-trash"></i></button>',
-	          				'</div>',
-	          			'</td>',
-	          		'</tr>');
+          			'<td>', tr_idx, '</td>',
+          			'<td>', task_name, '</td>',
+          			'<td>', start_time, '</td>',
+          			'<td>', end_time, '</td>',
+          			'<td>', task.totalDay, '</td>',
+          			is_active_tr_html,
+          			'<td>University</td>',
+          			'<td>', task.universityName, '</td>');
 				}
+				ret_html_array.push(
+					'<td>',
+	          			this_ptr.genTaskTbTrBtns(task.id, task_name),
+          			'</td>',
+          		'</tr>');
 				return ret_html_array.join("");
 			},
 			formTaskTbBodyHtml: function(task_array) {
@@ -146,6 +122,38 @@
 			}
 		};
 		
+		var TaskDeleteHelper = {
+			deleteTaskById: function(task_id, task_name) {
+				if(confirm("确定要删除任务“" + task_name + "”？")) {
+					var ids_array = [];
+					ids_array.push(task_id);
+					$.ajax({
+						url: "taskAction!deleteTaskByID.action",
+						data: {
+							ids: JSON.stringify(ids_array)
+						},
+						beforeSend: function() {
+							IHL_BlockMsgObj.showBlockMsg("<h1 style='font-size: 24px; line-height: 29px;'>Deleting Task ...<h1>");
+						},
+						success: function(rsps_data) {
+							var rsps_obj = $.parseJSON(rsps_data);
+							IHL_BlockMsgObj.unblockMsg(function() { 
+		                        if(rsps_obj.statusCode == "200") {
+		                        	// 刷新列表
+									$("#display-task-list").click();
+								} else {
+									$.growlUI('Error', rsps_obj.info);
+								}
+		                    });
+								
+						}
+					});
+					
+				}
+					
+			}
+		};
+		
 		$(document).on("click", "#display-task-list", function() {
 			var display_tl_btn = $(this);
 			$.ajax({
@@ -174,6 +182,9 @@
 		}).on("click", "#hide-task-list", function() {
 			$("#task-list-wrapper").slideUp("slow");
 			$("#display-task-list").fadeIn("slow");
+		}).on("click", ".delete-task-btn", function() {
+			var this_ele = $(this), task_id = this_ele.data("id"), task_name = this_ele.data("name");
+			TaskDeleteHelper.deleteTaskById(task_id, task_name);
 		});
 		
 	});
